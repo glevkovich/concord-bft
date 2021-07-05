7171e5c615dff9691f4c564e6448068d00e5c3f5
Gil Levkovich <glevkovich@vmware.com>
2021-07-05 11:07:24 +0300

    wp1

--- bftengine/tests/bcstatetransfer/test_app_state.hpp
+++ bftengine/tests/bcstatetransfer/test_app_state.hpp
@@ -56,7 +56,7 @@ class TestAppState : public IAppState {
     return true;
   };
 
-  bool putBlock(const uint64_t blockId, const char* block, const uint32_t blockSize) override {
+  bool putBlock(const uint64_t blockId, const char* block, const uint32_t blockSize, bool trylinkSTChainFrom) override {
     ConcordAssert(blockId < last_block_);
     Block bl;
     computeBlockDigest(blockId, block, blockSize, &bl.digest);
